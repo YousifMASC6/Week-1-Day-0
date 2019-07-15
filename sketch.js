36922575fd1b2f6faacf81409cbe5caeb6fe8b04
YousifMASC6 <52751353+YousifMASC6@users.noreply.github.com>
2019-07-15 13:57:45 -0400

    UPDATED SQUARES AND CIRCLES

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup(){
     createCanvas(500, 500)
 background(110, 800, 250)
-}
+var button = createButton("RESET")
+    button.position(8, 507);
+    button.mousePressed(setup);
+var button = createButton("ELLIPSE")
+    button.position(68, 507);
+    mouseDragged()
+var button = createButton("SQUARE")
+    button.position(138, 507);
+}   
 function mouseDragged() {
     ellipse(mouseX, mouseY, random([50], [100]));
     return false;
@@ -10,3 +18,8 @@ function draw() {
     rect(25, 25, 50, 50);
     fill(500, 200, 100);
 }
+
+    // function drawSquareClicked() {
+    //     drawSquare(mouseX, mouseY, random([50], [100]));
+    //     return false;
+    // }
